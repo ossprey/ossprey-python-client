0189a8c1a25b415a040f9fcbea3ac93a747bb837
David <31293924+dreadn0ught@users.noreply.github.com>
2025-01-27 18:12:32 +0000

    initial version

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 # Setup poetry
 pip install poetry==2.0.1
+poetry install
 
 # Git Config
 git config --global push.default current
---
+++ scan/sbom_javascript.py
@@ -0,0 +1,16 @@
+
+
+def create_sbom_from_npm():
+
+    # Run the following commands:
+    # get all versions of a package in the node_modules directory
+    # get all packages in the package-lock.json file
+    # npm install --dry-run --verbose
+
+    # Add all packages and versions to a SET
+    # Add each to the SBOM (with details where it was found)
+
+    # Add other info to the SBOM?
+    # Return the SBOM
+
+    return
