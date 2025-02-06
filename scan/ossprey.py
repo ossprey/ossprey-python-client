572bd1fd7f76f69df412c2ac861673c4502045b3
David <31293924+dreadn0ught@users.noreply.github.com>
2025-02-06 17:50:17 +0000

    Update ossprey to use new system (#8)

--- scan/ossprey.py
+++ scan/ossprey.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import requests
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -55,24 +56,59 @@ class Ossprey:
     # This takes a python dictionary and submits it to the API
     def validate(self, json_bom):
 
+        response = self.submit(json_bom)
+
+        match response.status_code:
+            case 200:
+                return response.json()
+            case 202:
+                job_id = response.json()['job_id']
+                return self.wait_for_completion(job_id)
+            case _:
+                logger.error("Failed to submit request")
+                logger.debug(f"Status code: {response.status_code}")
+                logger.debug(f"Response: {response.text}")
+
+                data = response.json()
+                if "message" in data:
+                    logger.error(data["message"])
+                return None
+
+    def submit(self, json_bom):
+
         # Get the url
-        url = self.api_url + '/ossprey'
+        url = self.api_url + '/submit'
 
         logger.debug(f"JSON Submission: {json.dumps(json_bom)}")
 
         # Submit bom to API
-        headers = {'Content-Type': 'application/json'}
-        headers['Authorization'] = f"Bearer {self.access_token}"
+        headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {self.access_token}"}
         response = requests.post(url, headers=headers, json=json_bom)
-        if response.status_code != 200:
-            logger.error("Failed to validate the BOM")
-            logger.debug(f"Status code: {response.status_code}")
-            logger.debug(f"Response: {response.text}")
 
-            data = response.json()
-            if "message" in data:
-                logger.error(data["message"])
-            return None
+        return response
+        
+    def wait_for_completion(self, job_id):
+        url = self.api_url + f'/status'
 
-        ret = response.json()
-        return ret
+        params = {"job_id": job_id}
+
+        headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {self.access_token}"}
+        for i in range(1, 20):
+            time.sleep(i * i)
+
+            response = requests.get(url, headers=headers, params=params)
+            if response.status_code not in [200, 202]:
+                logger.error("Error returned when retrieving the results")
+                logger.debug(f"Status code: {response.status_code}")
+                logger.debug(f"Response: {response.text}")
+                raise Exception("Error returned when retrieving the results")
+
+            ret = response.json()
+            if ret['status'] == 'SUCCEEDED':
+                if 'output' in ret:
+                    return ret['output']
+                else:
+                    raise Exception("Error no SBOM returned")
+
+        logger.error("Scan took too long to complete")
+        raise Exception("Scan took too long to complete")
