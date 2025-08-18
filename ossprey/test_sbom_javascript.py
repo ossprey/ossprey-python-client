2d03204adc77e9de870d57043da211ee7044be29
dreadn0ught <31293924+dreadn0ught@users.noreply.github.com>
2025-08-18 07:20:23 +0000

    fix typo

--- ossprey/test_sbom_javascript.py
+++ ossprey/test_sbom_javascript.py
@@ -4,12 +4,23 @@ from unittest.mock import patch, mock_open
 from typing import Any
 from pathlib import Path
 from ossprey.sbom_javascript import (
-    GitResolved,
-    exec_command, node_modules_directory_exists, find_package_json_files,
-    get_all_node_modules_packages, package_lock_file_exists, get_all_package_lock_packages,
-    package_json_file_exists, get_all_package_json_packages, run_npm_dry_run,
-    get_all_npm_dry_run_packages, yarn_lock_file_exists, get_all_yarn_lock_packages,
-    run_yarn_install, get_all_yarn_list_packages, update_sbom_from_npm, update_sbom_from_yarn
+    GitResolve,
+    exec_command,
+    node_modules_directory_exists,
+    find_package_json_files,
+    get_all_node_modules_packages,
+    package_lock_file_exists,
+    get_all_package_lock_packages,
+    package_json_file_exists,
+    get_all_package_json_packages,
+    run_npm_dry_run,
+    get_all_npm_dry_run_packages,
+    yarn_lock_file_exists,
+    get_all_yarn_lock_packages,
+    run_yarn_install,
+    get_all_yarn_list_packages,
+    update_sbom_from_npm,
+    update_sbom_from_yarn,
 )
 
 from ossbom.model.ossbom import OSSBOM
@@ -30,12 +41,16 @@ def test_node_modules_directory_exists(tmp_path: Path) -> None:
 def test_find_package_json_files(tmp_path: Path) -> None:
     (tmp_path / "node_modules").mkdir()
     (tmp_path / "node_modules" / "package.json").write_text("{}")
-    assert find_package_json_files(tmp_path) == [tmp_path / "node_modules" / "package.json"]
+    assert find_package_json_files(tmp_path) == [
+        tmp_path / "node_modules" / "package.json"
+    ]
 
 
 def test_get_all_node_modules_packages(tmp_path: Path) -> None:
     (tmp_path / "node_modules").mkdir()
-    (tmp_path / "node_modules" / "package.json").write_text('{"name": "testpkg", "version": "1.0.0"}')
+    (tmp_path / "node_modules" / "package.json").write_text(
+        '{"name": "testpkg", "version": "1.0.0"}'
+    )
     comps = get_all_node_modules_packages(tmp_path)
     assert len(comps) == 1
     c = comps[0]
@@ -50,7 +65,9 @@ def test_package_lock_file_exists(tmp_path: Path) -> None:
 
 
 def test_get_all_package_lock_packages(tmp_path: Path) -> None:
-    (tmp_path / "package-lock.json").write_text('{"packages": {"node_modules/testpkg": {"version": "1.0.0"}}}')
+    (tmp_path / "package-lock.json").write_text(
+        '{"packages": {"node_modules/testpkg": {"version": "1.0.0"}}}'
+    )
     comps = get_all_package_lock_packages(tmp_path)
     assert len(comps) == 1
     c = comps[0]
@@ -61,7 +78,9 @@ def test_get_all_package_lock_packages(tmp_path: Path) -> None:
 
 
 def test_get_all_package_lock_packages_ignore_blanks(tmp_path: Path) -> None:
-    (tmp_path / "package-lock.json").write_text('{"packages": {"": {}, "node_modules/testpkg": {"version": "1.0.0"}}}')
+    (tmp_path / "package-lock.json").write_text(
+        '{"packages": {"": {}, "node_modules/testpkg": {"version": "1.0.0"}}}'
+    )
     comps = get_all_package_lock_packages(tmp_path)
     assert len(comps) == 1
     c = comps[0]
@@ -77,14 +96,21 @@ def test_package_json_file_exists(tmp_path: Path) -> None:
 
 
 def test_get_all_package_json_packages(tmp_path: Path) -> None:
-    (tmp_path / "package.json").write_text('{"dependencies": {"testpkg": "1.0.0"}, "devDependencies": {"testpkg-dev": "1.0.0"}}')
-    assert get_all_package_json_packages(tmp_path) == [{"name": "testpkg", "version": "1.0.0"}, {"name": "testpkg-dev", "version": "1.0.0"}]
+    (tmp_path / "package.json").write_text(
+        '{"dependencies": {"testpkg": "1.0.0"}, "devDependencies": {"testpkg-dev": "1.0.0"}}'
+    )
+    assert get_all_package_json_packages(tmp_path) == [
+        {"name": "testpkg", "version": "1.0.0"},
+        {"name": "testpkg-dev", "version": "1.0.0"},
+    ]
 
 
 def test_run_npm_dry_run() -> None:
     with patch("ossprey.sbom_javascript.exec_command") as mock_exec_command:
         mock_exec_command.return_value = "add testpkg 1.0.0\n"
-        assert get_all_npm_dry_run_packages(".") == [{"name": "testpkg", "version": "1.0.0"}]
+        assert get_all_npm_dry_run_packages(".") == [
+            {"name": "testpkg", "version": "1.0.0"}
+        ]
 
 
 def test_yarn_lock_file_exists(tmp_path: Path) -> None:
@@ -110,7 +136,9 @@ def test_run_yarn_install() -> None:
 
 def test_get_all_yarn_list_packages() -> None:
     with patch("ossprey.sbom_javascript.exec_command") as mock_exec_command:
-        mock_exec_command.return_value = '{"data": {"trees": [{"name": "testpkg@1.0.0"}]}}'
+        mock_exec_command.return_value = (
+            '{"data": {"trees": [{"name": "testpkg@1.0.0"}]}}'
+        )
         comps = get_all_yarn_list_packages(".")
     assert len(comps) == 1
     c = comps[0]
@@ -132,9 +160,17 @@ def test_get_all_yarn_list_packages() -> None:
 
 
 def test_update_sbom_from_yarn() -> None:
-    with patch("ossprey.sbom_javascript.get_all_yarn_list_packages") as mock_get_all_yarn_list_packages:
+    with patch(
+        "ossprey.sbom_javascript.get_all_yarn_list_packages"
+    ) as mock_get_all_yarn_list_packages:
         mock_get_all_yarn_list_packages.return_value = [
-            Component.create(name="testpkg", version="1.0.0", env=None, type="npm", source="yarn list")
+            Component.create(
+                name="testpkg",
+                version="1.0.0",
+                env=None,
+                type="npm",
+                source="yarn list",
+            )
         ]
         sbom = OSSBOM()
         sbom = update_sbom_from_yarn(sbom, ".")
@@ -155,7 +191,9 @@ def test_gitresolved_parses_https_url() -> None:
 
 
 def test_gitresolved_parses_ssh_url() -> None:
-    gr = GitResolved("git+ssh://git@github.com/ossprey/example_malicious_javascript.git#deadbeef")
+    gr = GitResolved(
+        "git+ssh://git@github.com/ossprey/example_malicious_javascript.git#deadbeef"
+    )
     assert gr.get_type() == "github"
     assert gr.get_version() == "deadbeef"
     assert gr.url == "ssh://git@github.com/ossprey/example_malicious_javascript.git"
