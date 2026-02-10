56fa8166d461a307e43d501b4fcf21edc56e5be1
dreadn0ught <david.an.read@gmail.com>
2026-02-10 14:35:42 +0000

    fix tests

--- ossprey/test_sbom_python.py
+++ ossprey/test_sbom_python.py
@@ -4,14 +4,18 @@ from pathlib import Path
 import pytest
 from unittest.mock import patch
 
-from ossprey.sbom_python import create_sbom_from_env, create_sbom_from_requirements, get_cyclonedx_binary
+from ossprey.sbom_python import (
+    create_sbom_from_env,
+    create_sbom_from_requirements,
+    get_cyclonedx_binary,
+)
 from ossprey.virtualenv import VirtualEnv
 
 
 def test_get_sbom():
     sbom = create_sbom_from_env()
 
-    assert sbom.format == 'OSSBOM'
+    assert sbom.format == "OSSBOM"
 
 
 def test_get_sbom_from_venv():
@@ -20,16 +24,16 @@ def test_get_sbom_from_venv():
     venv.enter()
 
     # Install a package
-    venv.install_package('numpy')
+    venv.install_package("numpy")
 
     requirements_file = venv.create_requirements_file_from_env()
 
     # Get the SBOM
     sbom = create_sbom_from_requirements(requirements_file)
 
-    assert sbom.format == 'OSSBOM'
+    assert sbom.format == "OSSBOM"
     assert len(sbom.components) == 1
-    assert any(map(lambda x: x.name == 'numpy', sbom.components.values()))
+    assert any(map(lambda x: x.name == "numpy", sbom.components.values()))
 
 
 def test_get_sbom_from_venv_local_package():
@@ -38,16 +42,16 @@ def test_get_sbom_from_venv_local_package():
     venv.enter()
 
     # Install a package
-    venv.install_package('test/python_simple_math')
-  
+    venv.install_package("test/test_packages/python_simple_math")
+
     requirements_file = venv.create_requirements_file_from_env()
 
     # Get the SBOM
     sbom = create_sbom_from_requirements(requirements_file)
 
-    assert sbom.format == 'OSSBOM'
+    assert sbom.format == "OSSBOM"
     assert len(sbom.components) == 7
-    assert any(map(lambda x: x.name == 'simple_math', sbom.components.values()))
+    assert any(map(lambda x: x.name == "simple_math", sbom.components.values()))
 
 
 @patch("shutil.which")
